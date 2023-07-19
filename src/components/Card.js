15315bd72326d77f5524aa871b721372a6a4eb1a
Filip Mihocas <filip.mihocas@gmail.com>
2023-07-19 09:42:59 +0300

    finishing and simplifing the component props passing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
-      <img src={`../images/${props.img}`} className="card--image" alt="" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${props.coverImg}`} className="card--image" alt="" />
       <div className="card--stats">
         <img src="../images/star.svg" className="card--star" alt="" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
       <p>{props.title}</p>
