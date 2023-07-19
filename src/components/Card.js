4ce7f7ba294f0f88a1d3a2f1ce4f8c3dd2206f39
Filip Mihocas <filip.mihocas@gmail.com>
2023-07-19 08:58:51 +0300

    project setup

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className="card">
+      <img src={`../images/${props.img}`} className="card--image" alt="" />
+      <div className="card--stats">
+        <img src="../images/star.svg" className="card--star" alt="" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        <span className="bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
+}
